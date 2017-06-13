4b351b869e0b5ec60c01cc3da04d7391c8b4c9d1
LAPTOP-UG1B8O19\prate <prateeksurana3255@gmail.com>
2017-06-13 21:47:10 +0530

    todo list updated

--- Day11/app.js
+++ Day11/app.js
@@ -7,31 +7,81 @@ function firstCtrl(){
     todo.addTask=addTask;
     todo.editMode=false;
     todo.saveIndex=0;
+    todo.totalTasks=0;
+    todo.completedTasks=0;
+    cId=0;
     // taskObj={};
     
 
     function addTask(){
+        cId=cId+1;
         taskObj={};
         taskObj.name=todo.task;
         taskObj.status=0;
+        taskObj.id=cId;
         todo.tasks.push(taskObj);
         todo.task="";
+        todo.totalTasks=todo.totalTasks+1;
         console.log(todo.tasks);
     }
-    todo.deleteTask=function(i){
-        todo.tasks.splice(i,1);
+    todo.deleteTask=function(id){
+        todo.totalTasks=todo.totalTasks-1;
+        for(j=0;j<todo.tasks.length;j++){
+            if(todo.tasks[j].id==id)
+                break;
+        }
+        if(todo.tasks[j].status==1)
+            todo.completedTasks=todo.completedTasks-1;
+        todo.tasks.splice(j,1);
+        
     }
-    todo.editTask=function(i){
-        todo.task=todo.tasks[i].name;
+    todo.editTask=function(id){
+        for(j=0;j<todo.tasks.length;j++){
+            if(todo.tasks[j].id==id)
+                break;
+        }
+        todo.task=todo.tasks[j].name;
         todo.editMode=true;
-        todo.saveIndex=i;
+        todo.saveIndex=j;
     }
-    todo.updateTask=function(i){
+    todo.updateTask=function(){
+
         todo.tasks[todo.saveIndex].name=todo.task;
         todo.editMode=false;
         todo.task="";
     }
-    todo.setStatus=function(i){
-        todo.tasks[i].status=1;
+    todo.setStatus=function(id){
+        for(j=0;j<todo.tasks.length;j++){
+            if(todo.tasks[j].id==id)
+                break;
+        }
+        todo.tasks[j].status=1;
+        todo.completedTasks=todo.completedTasks+1;
+    }
+    todo.moveDown=function(id){
+        for(j=0;j<todo.tasks.length;j++){
+            if(todo.tasks[j].id==id)
+                break;
+        }
+        if(j!=todo.tasks.length-1){
+             var tempObject = todo.tasks.splice(j, 1, todo.tasks[j + 1])[0];
+             todo.tasks.splice(j+1, 1, tempObject);
+        }
+       
+        
+    }
+
+    todo.moveUp=function(id){
+        for(j=0;j<todo.tasks.length;j++){
+            if(todo.tasks[j].id==id)
+                break;
+        }
+        if(j!=0){
+             var tempObject = todo.tasks.splice(j-1, 1, todo.tasks[j])[0];
+             console.log(tempObject);
+             console.log(todo.tasks.splice(j, 1, tempObject));
+        }
+       
+        
     }
 }
