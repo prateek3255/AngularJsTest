68109b49ad4f741352de41943d2fe49a2d45fcf3
LAPTOP-UG1B8O19\prate <prateeksurana3255@gmail.com>
2017-06-17 18:27:52 +0530

    tic tac toe modified

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -13,16 +13,16 @@ function tic(){
     }
     t.player=true;
     t.win=0;
+    tie=true;
     t.changeState=function(id){
         if(t.buttons[id].state){
             if(t.player)
                 t.buttons[id].value="X";
             else
                 t.buttons[id].value="O";
+            t.player=!t.player;
         }
         t.buttons[id].state=false;
-        t.player=!t.player;
-        console.log(t.player);
         checkIfComplete();
 
     }
@@ -50,12 +50,55 @@ function tic(){
                 }
         }
 
+        if(t.buttons[0].value=='X'&&t.buttons[4].value=='X'&&t.buttons[8].value=='X'){
+                t.win=1;
+                disableAll();
+            }
+            else
+                if(t.buttons[0].value=='O'&&t.buttons[4].value=='O'&&t.buttons[8].value=='O'){
+                    t.win=2;
+                    disableAll();
+                }
 
+
+        if(t.buttons[2].value=='X'&&t.buttons[4].value=='X'&&t.buttons[6].value=='X'){
+                t.win=1;
+                disableAll();
+            }
+            else
+                if(t.buttons[2].value=='O'&&t.buttons[4].value=='O'&&t.buttons[6].value=='O'){
+                    t.win=2;
+                    disableAll();
+                }
+        console.log(allSelected(),tie);
+        if(allSelected()&&tie)
+            t.win=3;
     }
+
 
     function disableAll(){
         for(i=0;i<9;i++){
             t.buttons[i].state=false;
         }
+        tie=false;
+    }
+
+    function allSelected(){
+        flag=true;
+        for(i=0;i<9;i++){
+            if(t.buttons[i].state)
+                flag=false;
+        }
+        return flag;
+    }
+
+    t.reset=function(){
+        for(i=0;i<9;i++){
+            t.buttons[i].state=true;
+            t.buttons[i].value="";
+        }
+        t.win=0;
+        t.player=true;
+        tie=true;
     }
 } 
